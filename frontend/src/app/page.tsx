9afaaf8af6f4de4dbd7a77ddf15eb19f1f20d76c
Sina Niyavarzi <sinaniya@gmail.com>
2024-07-23 22:34:21 +0330

    added data type to frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,32 @@
-async function getData() {
-  const res = await fetch("http://localhost:8000");
+interface item {
+  done: boolean;
+  id: string;
+  title: string;
+  due: string;
+  modified: string;
+}
+
+type Data = item[];
+
+const getData = async () => {
+  const res = await fetch("http://localhost:8000/items");
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
 
   return res.json();
-}
+};
 
 export default async function Page() {
-  const data = await getData();
+  const data: Data = await getData();
 
-  return <main>{data["Hello"]}</main>;
+  return (
+    <main className="p-4">
+      <ul>
+        {data.map((d, i) => (
+          <li key={i}>{d.title}</li>
+        ))}
+      </ul>
+    </main>
+  );
 }
